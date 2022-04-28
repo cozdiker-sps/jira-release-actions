483b4fc6bbf1e96f8d2ffab01b0c0690ede08708
cozdiker-sps <79835257+cozdiker-sps@users.noreply.github.com>
2022-04-28 10:53:44 +0200

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -68,7 +68,7 @@ async function run(): Promise<void> {
         const versionToCreate: Version = {
           name: RELEASE_NAME,
           archived: false,
-          released: RELEASE,
+          released: false,
           releaseDate: new Date().toISOString(),
           projectId: Number(project.project?.id)
         }
@@ -80,7 +80,7 @@ async function run(): Promise<void> {
       const versionToUpdate: Version = {
         ...version,
         self: undefined,
-        released: RELEASE,
+        released: false,
         releaseDate: new Date().toISOString(),
         userReleaseDate: undefined
       }
